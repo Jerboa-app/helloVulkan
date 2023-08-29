c834629c1402e8f19739ea62cfb0fb577145e0eb
Jerboa-app <dev@jerboa.app>
2023-08-29 19:39:39 +0100

    add debug messenger

--- include/Renderer/vulkan.h
+++ include/Renderer/vulkan.h
@@ -39,10 +39,80 @@ namespace Renderer
 
             VkInstance instance;
 
+            VkDebugUtilsMessengerEXT debugMessenger;
+
             std::vector<VkLayerProperties> availableLayers;
+            std::vector<const char *> extensions;
 
             bool checkValidationLayerSupport();
             void supportedValidationLayers(bool print = false);
+
+            void getRequiredExtensions();
+
+            void setupDebugMessenger();
+            void destroyDebugMessenger();
+
+            static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback
+            (
+                VkDebugUtilsMessageSeverityFlagBitsEXT       messageSeverity,
+                VkDebugUtilsMessageTypeFlagsEXT              messageTypes,
+                const VkDebugUtilsMessengerCallbackDataEXT * pCallbackData,
+                void                                       * pUserData
+            )
+            {
+                /*
+                    messageSeverity can be
+
+                        VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:    Diagnostic message
+                        VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:       Informational message like the creation of a resource
+                        VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:    Message about behavior that is not necessarily an error, but very likely a bug in your application
+                        VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:      Message about behavior that is invalid and may cause crashes
+
+                    messageType can be
+
+                        VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT:        Some event has happened that is unrelated to the specification or performance
+                        VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT:     Something has happened that violates the specification or indicates a possible mistake
+                        VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT:    Potential non-optimal use of Vulkan
+
+                */
+                std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+                // abort triggering call
+                return VK_FALSE;
+            }
+
+            // proxy to create a debug messenger
+
+            VkResult createDebugUtilsMessengerEXT
+            (
+                VkInstance instance, 
+                const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, 
+                const VkAllocationCallbacks* pAllocator, 
+                VkDebugUtilsMessengerEXT* pDebugMessenger
+            )
+            {
+                auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+                if (func != nullptr) {
+                    return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+                } else {
+                    return VK_ERROR_EXTENSION_NOT_PRESENT;
+                }
+            }
+
+            // proxy to destroy a debug messenger
+
+            static void destroyDebugUtilsMessengerEXT
+            (
+                VkInstance instance, 
+                VkDebugUtilsMessengerEXT debugMessenger, 
+                const VkAllocationCallbacks* pAllocator
+            ) 
+            {
+                auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+                if (func != nullptr) {
+                    func(instance, debugMessenger, pAllocator);
+                }
+            }
     };
 
 }
--- src/Renderer/vulkan.cpp
+++ src/Renderer/vulkan.cpp
@@ -13,10 +13,12 @@ namespace Renderer
         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.pEngineName = "No engine";
         appInfo.engineVersion = VK_API_VERSION_1_0;
+        appInfo.pNext = nullptr;
         
         VkInstanceCreateInfo createInfo;
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
+        createInfo.pNext = nullptr;
 
         if (enableValidationLayers)
         {
@@ -28,14 +30,10 @@ namespace Renderer
             createInfo.enabledLayerCount = 0;
         }
 
-        // glfw 
-        uint32_t glfwExtensionCount = 0;
-        const char ** glfwExtensions;
+        getRequiredExtensions();
 
-        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
-        createInfo.enabledExtensionCount = glfwExtensionCount;
-        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledExtensionCount = extensions.size();
+        createInfo.ppEnabledExtensionNames = extensions.data();
 
         if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
         {
@@ -46,10 +44,18 @@ namespace Renderer
         {
             throw std::runtime_error("Validation layers are not supported");
         }
+
+        setupDebugMessenger();
     }
 
     VulkanRenderer::~VulkanRenderer()
     {
+
+        if (enableValidationLayers)
+        {
+            destroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+        }
+
         vkDestroyInstance(instance, nullptr);
     }
 
@@ -79,6 +85,22 @@ namespace Renderer
         return true;
     }
 
+    void VulkanRenderer::getRequiredExtensions()
+    {
+        // glfw 
+        uint32_t glfwExtensionCount = 0;
+        const char ** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        extensions = std::vector<const char *>(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+        if (enableValidationLayers)
+        {
+            extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        }
+    }
+
     void VulkanRenderer::supportedValidationLayers(bool print)
     {
         uint32_t layerCount;
@@ -96,5 +118,26 @@ namespace Renderer
             }
         }
     }
+
+    void VulkanRenderer::setupDebugMessenger()
+    {
+        if (!enableValidationLayers) { return; }
+
+        VkDebugUtilsMessengerCreateInfoEXT createInfo;
+        createInfo.sType =              VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        createInfo.messageSeverity =    VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        createInfo.messageType =        VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        createInfo.pfnUserCallback =    debugCallback;
+        createInfo.pUserData =          nullptr; // Optional
+        createInfo.pNext = nullptr;
+        createInfo.flags = 0;
+
+        if (createDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS)
+        {
+            throw std::runtime_error("failed to set up debug messenger!");
+        }
+    }
+
+    void destroyDebugMessenger();
 
 }
